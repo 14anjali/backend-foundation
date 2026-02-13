b5de0e7cedb1d67a8ffef66a6cccb88961c9c7a4
anjali <1424anjali@gmail.com>
2026-02-13 21:59:05 +0530

    chore: initialize TypeScript project with tsconfig and strict mode feat: practice primitive types and any vs unknown

--- 01-basics/index.ts
+++ 01-basics/index.ts
@@ -1,2 +1,7 @@
-let message: string = "Hello TypeScript";
-console.log(message);
+// let message:string="Hello World !"
+// console.log(message)
+
+//day 2
+
+let name:string="anjali"
+console.log(name)
---
+++ 02-basic-Types/Data-types.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//String type
+var name = "Anjali";
+console.log(name);
+//Number type
+var age = 22;
+console.log(age);
+//Boolean type
+var loggedIn = true;
+console.log(name, age, loggedIn);
